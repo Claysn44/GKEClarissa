aef2f5fc29f55fa67cd16234f325c557d809d0c1
Claysn44 <149597232+Claysn44@users.noreply.github.com>
2024-04-16 13:41:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Clarissa Yason - 52021003 - Teknik Informatika')
+    res.send('Tes')
 })
 
 app.listen(PORT, HOST)
